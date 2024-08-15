b31906a231b5e64a6849c1bf3d50ae5d055ab0cf
Asma-Malick <165016510+Asma-Malick@users.noreply.github.com>
2024-08-15 19:34:33 +0530

    Delete print.py

--- print.py
+++
@@ -1,8 +0,0 @@
-name=input("what is your name? ")
-name2=len(name)
-if name2 < 3:
-  print("your name is too short")
-elif name2 >50:
-  print("your name is too long")
-else:
-  print("your name is just right")
